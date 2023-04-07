820abd56f8be8b570c7114a516348f569418c513
jsminfo <130086900+jsminfo@users.noreply.github.com>
2023-04-07 21:14:36 +0200

    Update streamlit_app.py
    added panda normalize

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,4 +29,8 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
+# gibt text in besser lesbar zurück
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Ausgabe
+streamlit.dataframe(fruityvice_normalized)
 
